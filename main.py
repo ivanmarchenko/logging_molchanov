8dbe8967b391a2257997562c7d3b383f7acebbf7
ivanmarchenko <marchenkoivanu@gmail.com>
2020-11-03 15:49:33 +0300

    4

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ logging.config.dictConfig(logger_config)
 
 logger = logging.getLogger('app_logger')
 
+words = ['new house', 'apple', 'ice cream' ]
+
 def main():
-    logger.debug('Enter')
+    for item in words:
+        try:
+            print(item.split(' '))
+        except Exception as err:
+            # logger.debug(f 'Exception here: {err}')
+            # logger.debug(f'Exception here, item={item}', exc_info=True)
+            logger.exception(f'Exception here, item={item}')
+
+            pass 
 
 
 if __name__ == "__main__": 
